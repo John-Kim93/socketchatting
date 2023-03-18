8ab83d6032a3fdb09108da027e06e8cf962df400
John-Kim93 <dhkdhd123@naver,com>
2023-03-18 13:12:22 +0900

    FEAT Home and Talk page
    1. binding input data(userName)
    2. navigate to Talk page
    3. show error message when user clicks enter button without typing userName
    4. make Talk.js
    5. etc.

--- Client_user/client-user/src/pages/Home.js
+++ Client_user/client-user/src/pages/Home.js
@@ -1,13 +1,35 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 import "./Home.css"
 
 export default function Home() {
+  let navigate = useNavigate();
+
+  const [userName, setUserName] = useState("")
+  const [waring, setWarning] = useState(false)
+  const enterName = () => {
+    if (userName) {
+      sessionStorage.setItem('userName', userName)
+      navigate("/talk")
+    } else {
+      setWarning(true)
+    }
+  }
+  
   return(
     <>
       <div className="container"> 
         <input
+          name="userName"
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.target.value)
+            setWarning(false)
+          }}
           placeholder="이름을 입력하세요."
         />
-        <button>Enter</button>
+        <button onClick={enterName}>Enter</button>
+        {waring && <p>이름을 입력하세요!</p>}
       </div>
     </>
   )
---
+++ Client_user/client-user/src/pages/Talk.js
@@ -0,0 +1,7 @@
+export default function Talk() {
+  return(
+    <>
+      talk talk
+    </>
+  )
+}
