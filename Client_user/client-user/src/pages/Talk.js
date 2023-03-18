8c9c681de48e531f2c04ac04a3d64cda31dec08f
John-Kim93 <dhkdhd123@naver,com>
2023-03-18 14:44:14 +0900

    FEAT basic client - server communication(echo)

--- Client_user/client-user/src/pages/Talk.js
+++ Client_user/client-user/src/pages/Talk.js
@@ -1,18 +1,40 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import "./Talk.css"
 
 export default function Talk() {
   const [sendCount, setsendCount] = useState(0)
   const [typingText, setTypingText] = useState("")
   const [printedText, setPrintedText] = useState([])
+  
+  const wsRef = useRef(null);
+  useEffect(() => {
+    wsRef.current = new WebSocket("ws://localhost:8100")
+
+    wsRef.current.onopen = () => {
+      console.log("web socket 연결!!!")
+    };
+
+    wsRef.current.onmessage = (event) => {
+      setPrintedText(prevPrintedText => [...prevPrintedText, event.data])
+    };
+
+    return () => {
+      console.log("web socket 연결 끝!!!")
+      wsRef.current.close()
+    };
+  }, [])
+
   const user = sessionStorage.getItem("userName")
+
   const sendMessage = () => {
     if (typingText) {
       setsendCount(sendCount + 1)
       setPrintedText([...printedText, `${user} : ${typingText}`])
       setTypingText("")
+      wsRef.current.send(typingText)
     }
   }
+  
   return(
     <div className="container">
       <textarea
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # socketchatting
-클라이언트 실행 : Client_user/client-user에서 npm start 
+클라이언트 실행 : Client_user/client-user에서 npm start 입력
+
+테스트 서버 실행 : node_test_server에서 npm i ws 이후 node WebSocketEchoServer.js 입력
