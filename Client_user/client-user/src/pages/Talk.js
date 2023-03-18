d2816d663b7408b9ad242220c12ccfc20cf83ce0
John-Kim93 <dhkdhd123@naver,com>
2023-03-18 14:03:44 +0900

    FEAT chatting alone

--- Client_user/client-user/src/pages/Talk.js
+++ Client_user/client-user/src/pages/Talk.js
@@ -1,7 +1,28 @@
+import { useState } from "react"
+import "./Talk.css"
+
 export default function Talk() {
+  const [sendCount, setsendCount] = useState(0)
+  const [typingText, setTypingText] = useState("")
+  const [printedText, setPrintedText] = useState([])
+  const user = sessionStorage.getItem("userName")
+  const sendMessage = () => {
+    if (typingText) {
+      setsendCount(sendCount + 1)
+      setPrintedText([...printedText, `${user} : ${typingText}`])
+      setTypingText("")
+    }
+  }
   return(
-    <>
-      talk talk
-    </>
+    <div className="container">
+      <textarea
+        name="typingText"
+        value={typingText}
+        onChange={(e) => {setTypingText(e.target.value)}}
+      />
+      <button onClick={sendMessage}>보내기</button>
+      <p>보낸 메시지 횟수 : {sendCount}번</p>
+      {printedText.map((message, idx) => <div className="message" key={idx}>{message}</div>)}
+    </div>
   )
 }
