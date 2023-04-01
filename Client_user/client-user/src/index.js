2f27cd86d5edd2eab2018a30bcb00428180fd617
John-Kim93 <dhkdhd123@naver,com>
2023-04-01 22:16:17 +0900

    FEAT set recoil

--- Client_user/client-user/src/index.js
+++ Client_user/client-user/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from "./Router";
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router />
+    <RecoilRoot>
+      <Router />
+    </RecoilRoot>
   </React.StrictMode>
 );
